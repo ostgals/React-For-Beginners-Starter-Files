8b227da51ad291d7bdc6f3cfc810ebc12ebda14e
Andrejs Kuzmins <ostgals@gmail.com>
2018-03-02 12:06:44 +0200

    after video 14

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -5,6 +5,7 @@ const Inventory = (props) => (
   <div className="inventory">
     <h2>Inventory</h2>
     <FishForm addFish={props.addFish} />
+    <button onClick={props.loadSampleFishes}>Load Sample Fishes</button>
   </div>
 );
 
